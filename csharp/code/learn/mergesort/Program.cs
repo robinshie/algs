30a84e2cba3f6e888009503a8fe82c6c6b576711
robin code <robin.shie@outlook.com>
2020-03-12 20:32:56 +0800

    add c#
    add c# learned

---
+++ csharp/code/learn/mergesort/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    { 
+        static int count=0;
+        static void Main(string[] args)
+        {
+            int[] A = new int[]{8,7,6,5,4,3,2,1};
+            int[] temp = new int[A.Length];
+
+            MeragedSort(A,0,A.Length-1,temp);
+            System.Console.WriteLine(count);
+            foreach(var i in A)
+            {
+                Console.Write(i+",");
+            }
+        }
+
+        private static void MeragedSort(int[] A, int start, int end, int[] temp)
+        {
+            System.Console.WriteLine(start+"->"+end);
+            if(start>=end)
+            {
+                return;
+            }
+            int mid = (start+end)/2;
+            MeragedSort(A,start,mid,temp);
+            MeragedSort(A,mid+1,end,temp);
+            Merage(A,start,end,temp);
+
+        }
+
+        private static void Merage(int[] A, int start, int end, int[] temp)
+        {
+            int mid =  (start+end)/2;
+            count++;
+            int leftStart = start;
+            int rightStart = mid+1;
+            int index = leftStart;
+            while(leftStart<=mid && rightStart<=end)
+            {
+                if(A[leftStart]<=A[rightStart])
+                {
+                    temp[index++]=A[leftStart++];
+                }
+                else
+                {
+                    temp[index++]=A[rightStart++];
+                }
+            }
+            while (leftStart<=mid)
+            {
+                temp[index++]=A[leftStart++];
+            }
+            while (rightStart<=end)
+            {
+                temp[index++]=A[rightStart++];
+            }
+
+            for (int i = start; i <= end; i++)
+            {
+                A[i] = temp[i];
+            }
+        
+        }
+    }
+}
---
+++ csharp/code/learn/quicksort/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+namespace quicksort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[]{3,4,1,5,2};
+            quicksortMethod(A,0,A.Count()-1);
+            foreach (var item in A)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+
+        private static void quicksortMethod(int[] A, int start, int end)
+        {
+            System.Console.WriteLine($"start:{start},end:{end}");
+            if(start>=end)
+            {
+                return;
+            }
+            int pivol = A[(start+end)/2];
+            System.Console.WriteLine($"pivol:{pivol}");
+            int left = start;int right = end;
+            while(left<right) //start 和end 部分不能有交集
+            {
+                
+            System.Console.WriteLine($"left：{left},right：{right}");
+                while (left<right && A[left]<pivol)
+                {
+                    left++;
+                }
+                while (left<right && A[right] > pivol)
+                {
+                    right--;
+                }
+                if(left<right)
+                {
+                    int temp= A[right];
+                    A[right] = A[left];
+                    A[left] = temp;
+                    left++;
+                    right--;
+                }
+
+            }
+            quicksortMethod(A,start,right);
+            quicksortMethod(A,left,end);
+        }
+    }
+}
